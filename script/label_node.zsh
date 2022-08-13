f56f61c137d2ce7f15fefcda354917067ee68e3c
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-08-14 08:20:01 +0900

    feat: 環境センサーを追加

--- script/label_node.zsh
+++ script/label_node.zsh
@@ -1,2 +1,11 @@
 kubectl label node tanzania node-role.kubernetes.io/server=large --overwrite
+
+
+kubectl label node rpi-cm4-sensor-1 node-role.kubernetes.io/sensor=room --overwrite
+kubectl annotate nodes rpi-cm4-sensor-1 location=洋室A --overwrite
+kubectl label node rpi-cm4-sensor-2 node-role.kubernetes.io/sensor=room --overwrite
+kubectl annotate nodes rpi-cm4-sensor-2 location=洋室B --overwrite
+kubectl label node rpi-cm4-sensor-3 node-role.kubernetes.io/sensor=room --overwrite
+kubectl annotate nodes rpi-cm4-sensor-3 location=洋室C --overwrite
 kubectl label node rasp-cooler node-role.kubernetes.io/hems=unit_cooler --overwrite
+kubectl annotate nodes rasp-cooler location=室外機冷却 --overwrite
