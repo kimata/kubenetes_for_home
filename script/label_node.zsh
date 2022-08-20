dcc7345160d2c13d9dcb1c0353a8a78b208a7e09
KIMATA Tetsuya <kimata@green-rabbit.net>
2022-08-20 23:40:40 +0900

    fix: hems サーバー追加

--- script/label_node.zsh
+++ script/label_node.zsh
@@ -12,6 +12,7 @@ kubectl annotate nodes rpi-cm4-sensor-3 location=洋室C --overwrite
 kubectl label node rpi-cm4-sensor-4 node-role.kubernetes.io/sensor=room --overwrite
 kubectl annotate nodes rpi-cm4-sensor-4 location=家事室 --overwrite
 
+kubectl label node hems node-role.kubernetes.io/server=hems --overwrite
 
 kubectl label node rasp-meter-8 node-role.kubernetes.io/sensor=room --overwrite
 kubectl annotate nodes rasp-meter-8 location=監視カメラボックス --overwrite
