f3d959bb72a36e6b9153670c716c4640002639e1
KIMATA Tetsuya <kimata@green-rabbit.net>
2023-05-09 14:55:03 +0900

    fix: バージョンアップ用スクリプトを追加

---
+++ script/kube_update.zsh
@@ -0,0 +1,32 @@
+#!/usr/bin/env zsh
+
+run() {
+    echo "\e[0;36mrunning \e[1;37m\`$@'\e[0m"
+    $@
+    echo
+}
+
+run sudo curl -so /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+run sudo apt update
+
+echo -e "\e[0;32mインストールするバージョンを選択してください．\e[0m"
+select ver in $(apt-cache madison kubeadm | head -n 4 | cut -d '|' -f 2 | xargs)
+do
+    break
+done
+
+echo -ne "\e[0;32mバージョン \033[1;33m${ver} \033[0;32mに更新しますか？\e[0m"
+read -q "A? [y/N] "
+echo
+if [ $A != "y" ]; then 
+    echo "Stop".
+    exit
+fi
+
+echo -e "\e[0;32mバージョン \033[1;33m${ver} への更新を開始します．\e[0m"
+for name in kubeadm kubelet kubectl;
+do
+    run sudo apt-mark unhold $name
+    run sudo apt-get install --allow-downgrades -y $name=$ver
+    run sudo apt-mark hold $name
+ done
